79cc1154bd8345b36c4828ff8456f2a2a1c50275
Ryan Santoyo <ryansantoyo3@gmail.com>
2026-03-05 09:33:24 +0800

    Remodeling

--- obj/Debug/net9.0/jvPo.AssemblyInfo.cs
+++ obj/Debug/net9.0/jvPo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("jvPo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d937928cc3056917848288b152de210dac4fc51e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9adc3eef9c2a46d9d0a57e4428874614c42636b")]
 [assembly: System.Reflection.AssemblyProductAttribute("jvPo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("jvPo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
