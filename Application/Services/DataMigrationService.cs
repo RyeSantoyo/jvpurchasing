c9a23e19e9b3143ef3aae481692f4b072eb2c45a
Ryan Santoyo <ryansantoyo3@gmail.com>
2026-04-17 15:47:48 +0800

    PO Migration == done

--- Application/Services/DataMigrationService.cs
+++ Application/Services/DataMigrationService.cs
@@ -29,64 +29,74 @@ namespace jvPo.Application.Services
             // var legacyRows = await legacyConn.QueryAsync<LegacyPO>(@"SELECT id, CompId, PONO, date1, suppid, 
             //             suppname, address, terms, requestedby, ronum, delto, date2, totalamount, remarks, orderby, preparedby");
             var legacyRows = await legacyConn.QueryAsync<LegacyPO>(@"SELECT * From PO");
-            var companyDict = await _context.Companies.ToDictionaryAsync(c => c.CompanyCode, c => c.Id);
-            
+            var companyDict = await _context.Companies.ToDictionaryAsync(c => c.CompanyCode.Trim(), c => c.Id);
+            var suppDict = await _context.Suppliers.ToDictionaryAsync(s => s.SupplierName.Trim().ToLower(), s => s.Id);
+            var termsDict = await _context.Terms.ToDictionaryAsync(t => t.Term.Trim().ToLower(), t => t.Id);
+            var addressDict = await _context.DeliveryAddresses.ToDictionaryAsync(t=> t.Address.Trim().ToLower(), t=> t.Id);
+
             int processedCount = 0;
             int batchSize = 500;
             foreach (var oldItem in legacyRows)
             {
                 string searchPONO = oldItem.PONO.ToString("G0");
                 string legacyCompId = oldItem.CompID.ToString();
+                string legacySuppName = oldItem.suppname?.Trim().ToLower() ?? "";
+                string legacyTerms = oldItem.terms?.Trim().ToLower() ?? "";
+                string legacyAddress = oldItem.delto?.Trim().ToLower() ?? "";
                 //int legacySupid = (int)oldItem.suppid;
 
-                if (companyDict.TryGetValue(legacyCompId, out int newCompanyId))
+                if (companyDict.TryGetValue(legacyCompId, out int newCompanyId) && suppDict.TryGetValue(legacySuppName, out int newSuppId))
                 {
-                    bool exists = await _context.POs.AnyAsync(x => x.PONumber == searchPONO);
-                    if (!exists)
+                    if (addressDict.TryGetValue(legacyAddress, out int newAddressId))
                     {
-                        var newItem = new PO
+                        bool exists = await _context.POs.AnyAsync(x => x.PONumber == searchPONO);
+                        if (!exists)
                         {
-                            PONumber = searchPONO,
-                            PODate = oldItem.date1,
-                            SupplierId = 1,
-                            SupplierName = oldItem.suppname,
-                            SupplierAddress = oldItem.address,
-                            AgreedTerms = oldItem.terms,
-                            TermsId = 1,
-                            RequestedBy = oldItem.requestedby,
-                            RONumber = (int)oldItem.ronum,
-                            DeliveryAddress = oldItem.delto,
-                            DeliveryAddressID = 1,
-                            RODate = oldItem.date2,
-                            TotalAmount = oldItem.totalamount,
-                            Remarks = oldItem.remarks,
-                            OrderBy = oldItem.orderby,
-                            CompanyCode = newCompanyId.ToString(),
+                            termsDict.TryGetValue(legacyTerms, out int newTermsId);
+                            var newItem = new PO
+                            {
+                                PONumber = searchPONO,
+                                PODate = oldItem.date1,
+                                SupplierId = newSuppId,
+                                SupplierName = oldItem.suppname,
+                                SupplierAddress = oldItem.address,
+                                AgreedTerms = oldItem.terms,
+                                TermsId = newTermsId != 0 ? newTermsId :1,
+                                RequestedBy = oldItem.requestedby,
+                                RONumber = (int)oldItem.ronum,
+                                DeliveryAddress = oldItem.delto,
+                                DeliveryAddressID = newAddressId,
+                                RODate = oldItem.date2,
+                                TotalAmount = oldItem.totalamount,
+                                Remarks = oldItem.remarks,
+                                OrderBy = oldItem.orderby,
+                                CompanyCode = newCompanyId.ToString(),
 
-                            CompanyId = newCompanyId
+                                CompanyId = newCompanyId
 
-                        };
-                        _context.POs.Add(newItem);
-                        processedCount++;
+                            };
+                            _context.POs.Add(newItem);
+                            processedCount++;
 
-                        if (processedCount % batchSize == 0)
-                        {
-                            await _context.SaveChangesAsync();
-                            _context.ChangeTracker.Clear();
+                            if (processedCount % batchSize == 0)
+                            {
+                                await _context.SaveChangesAsync();
+                                _context.ChangeTracker.Clear();
+                            }
                         }
                     }
 
-                }
-                else
-                {
-                    Console.WriteLine($"Company Code {legacyCompId} not found in new database. Skipping PO with PONumber {searchPONO}.");
-                }
-                // var parentHeader = await _context.POs.FirstOrDefaultAsync(x => x.PONumber == searchPONO);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Company Code {legacyCompId} not found in new database. Skipping PO with PONumber {searchPONO}.");
+                    }
+                    // var parentHeader = await _context.POs.FirstOrDefaultAsync(x => x.PONumber == searchPONO);
 
+                }
+
+                await _context.SaveChangesAsync();
+                return processedCount;
             }
-
-            await _context.SaveChangesAsync();
-            return processedCount;
         }
     }
-}
--- Models/Company.cs
+++ Models/Company.cs
@@ -8,7 +8,7 @@ namespace jvPo.Models
     public class Company
     {
         public int Id { get; set; }
-        public string? CompanyCode { get; set; }
+        public string CompanyCode { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         
     }
