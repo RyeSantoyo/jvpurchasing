f5506d015980ddf4f3ec27c95c14bfe83ad6353b
Ryan Santoyo <ryansantoyo3@gmail.com>
2026-05-28 16:01:07 +0800

    WebHook Test

--- Report/ViewPODetails.cs
+++ Report/ViewPODetails.cs
@@ -12,5 +12,13 @@ namespace jvPo.Report
         {
             InitializeComponent();
         }
+
+        public ViewPODetails(string poNumber)
+        {
+            InitializeComponent();
+            // You can use the poNumber to fetch data and set it as the DataSource for the report
+            // For example:
+            // this.DataSource = FetchPODetails(poNumber);
+        }
     }
 }
